f3d91accad86f1e932475a8c63f25cd3820c1553
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2021-12-11 23:52:04 +0000

    GRMFPF

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
-import { Component } from "react";
+import React,{useEffect} from "react";
 
 //ReactGA.initialize('UA-215064440-1');
 //ReactGA.pageview('test-init-pageview');
 
 
 
-class App extends Component {
+export default function App() {
 
-  componentDidMount(){
-    ReactGA.initialize(`UA-215064440-1`);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }
+  useEffect(() => {
+    ReactGA.initialize('UA-215064440-1');
+    ReactGA.pageview(window.location.pathname);
+  })
 
-  render() {
-    return (
-      <div className="App container">
-        <img src={logo} alt="logo" />
-        <h2>Herewegoagain</h2>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>GRMFPF</h1>
+    </div>
+  );
 }
-export default App;
