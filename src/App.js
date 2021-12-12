256086ab199eb32bf980eb880fb87f4ee59d9df6
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2021-12-12 11:47:18 +0000

    MVP Completed!!!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
-import { Component } from "react";
-
+import React, { Component } from "react";
+import reportWebVitals from './reportWebVitals';
+import { Hook, Console, Decode } from 'console-feed'
 //ReactGA.initialize('UA-215064440-1');
 //ReactGA.pageview('test-init-pageview');
 
-
-
+reportWebVitals(console.log)
 class App extends Component {
+  state = {
+    logs: []
+  }
 
   initReactGA = () => {
     ReactGA.initialize('UA-215064440-1',
@@ -20,13 +23,27 @@ class App extends Component {
 
   componentDidMount(){
     this.initReactGA();
+    Hook(window.console, log => {
+      this.setState(({ logs }) => ({ logs: [...logs, Decode(log)] }))
+    })
+    console.log(`Hello world!!!!`)
+    ReactGA.pageview('/I/am/alive');
+    reportWebVitals();
   }
 
   render() {
-    return (
+    return (            
       <div className="App container">
-        <img src={logo} alt="logo" />
-        <h2>Testing Out GA For Funzies</h2>
+        <img className="App-logo" src={logo} alt="logo" />
+        <h1>Google Analytics Toy</h1>
+        <p>Basic Toy to try out Google Analytics with React</p>
+        <p>Based on mostly boiler code from a number of sources:</p>
+        <p><a href="https://www.npmjs.com/package/react-ga">react-ga NodeJS Library</a></p>
+        <p><a href="https://www.npmjs.com/package/console-feed">console-feed NodeJS library</a></p>
+        <div className="App-console" style={{ backgroundColor: '#242424' }}>
+          <Console logs={this.state.logs} variant="dark" />
+        </div>
+
       </div>
     );
   }
