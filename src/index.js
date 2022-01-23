21b0c1d87686f3f0f81a47b1f6cce2d00720e71a
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2022-01-23 21:18:40 +0000

    Added navigation... now we see if we get magic...

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
+const rootElement = document.getElementById("root"); 
+//Stripping this out to a constant to make the render more readable
 
 ReactDOM.render(
   
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> 
+    {/*
+    Browser Router is required for react-router-dom.
+    Wrapping App with this is required for routing to work. 
+    */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
   
-  document.getElementById('root')
+  rootElement
 );
